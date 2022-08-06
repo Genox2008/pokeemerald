8cda6beaf6614f05b1f39c44138ae584e2ff8052
Genox2008 <drinemini@outlook.de>
2022-08-06 23:42:45 +0200

    FIrst commit

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1055,3 +1055,14 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Route1/scripts.inc"
+
+
+	.include "data/maps/Route2/scripts.inc"
+
+	.include "data/maps/Route3/scripts.inc"
+
+	.include "data/maps/PlayerHouse_2F/scripts.inc"
+
+	.include "data/maps/PlayerHouse_1F/scripts.inc"
