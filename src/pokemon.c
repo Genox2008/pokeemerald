0d0d4d86c928cc546c135d27b0960dd38841a666
Voluptua <Drinemini@outlook.de>
2023-09-04 21:25:21 +0200

    Added functionality for a IV Checker NPC

--- src/pokemon.c
+++ src/pokemon.c
@@ -7265,3 +7265,28 @@ u8 *MonSpritesGfxManager_GetSpritePtr(u8 managerId, u8 spriteNum)
         return gfx->spritePointers[spriteNum];
     }
 }
+
+u16 GetHpIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_HP_IV, NULL);
+}
+u16 GetAtkIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_ATK_IV, NULL);
+}
+u16 GetDefIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_DEF_IV, NULL);
+}
+u16 GetSpAtkIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPATK_IV, NULL);
+}
+u16 GetSpDefIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPDEF_IV, NULL);
+}
+u16 GetSpeedIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPEED_IV, NULL);
+}
