2ec2bed6a112dc19efd4a3d19d7e40f596fc8ba9
voluptua <Drinemini@outlook.de>
2024-03-24 18:16:41 +0100

    Added IV Checker

--- src/pokemon.c
+++ src/pokemon.c
@@ -6228,3 +6228,28 @@ u16 GetSpeciesPreEvolution(u16 species)
 
     return SPECIES_NONE;
 }
+
+u16 GetHpIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_HP_IV, NULL);
+}
+u16 GetAtkIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_ATK_IV, NULL);
+}
+u16 GetDefIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_DEF_IV, NULL);
+}
+u16 GetSpAtkIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPATK_IV, NULL);
+}
+u16 GetSpDefIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPDEF_IV, NULL);
+}
+u16 GetSpeedIV(void)
+{
+	return GetMonData(&gPlayerParty[GetLeadMonIndex()], MON_DATA_SPEED_IV, NULL);
+}
