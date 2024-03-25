d8e68233b279631ba8b511add8f57e916835b73e
voluptua <Drinemini@outlook.de>
2024-03-25 20:21:44 +0100

    Fixed Surf Dismount Ground Effects

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1731,6 +1731,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
